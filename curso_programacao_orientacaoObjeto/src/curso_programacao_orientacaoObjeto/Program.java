480e3fad918ed56a050820940c49d053a5e3d322
Poscidonio <guilhermeposcidonio@gmail.com>
2022-03-28 22:51:10 -0300

    Encapsulamento

--- curso_programacao_orientacaoObjeto/src/curso_programacao_orientacaoObjeto/Program.java
+++ curso_programacao_orientacaoObjeto/src/curso_programacao_orientacaoObjeto/Program.java
@@ -13,15 +13,18 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		
-		Product product = new Product();
 		System.out.println("Enter product data: ");
 		System.out.print("Name: ");
-		product.name = sc.nextLine();
+		String name = sc.nextLine();
 		System.out.print("Price: ");
-		product.price = sc.nextDouble();
-		System.out.print("Quantity in stock: ");
-		product.quantity = sc.nextInt();
+		double price = sc.nextDouble();
+		Product product = new Product(name, price);
+		
+		//encapsulamento
+		product.setName("Computer");
+		System.out.println("Update name: " + product.getName());
+		product.setPrice(1200.00);
+		System.out.println("Update price: " + product.getPrice());
 		
 		System.out.println("\n Product data: " + product + "\n");
 		
--- curso_programacao_orientacaoObjeto/src/entities/Product.java
+++ curso_programacao_orientacaoObjeto/src/entities/Product.java
@@ -2,11 +2,56 @@ package entities;
 
 public class Product {
 
-	// atributos da classef
-	public String name;
-	public double price;
-	public int quantity;
+	// atributos da classe
+	private String name;
+	private double price;
+	private int quantity;
 
+	public Product() {
+
+	}
+
+	// contrutor nao tem tipo de retorno e executa a instanciação do objeto(obriga a
+	// receber os parametros)
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+
+	}
+
+	// sobre carga quando ha mais de uma opção para o mesmo contrutor com parametros
+	// diferentes
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+		// nao é necessario declarar ja que o contrutor nao esta pedindo
+		// this.quantity = quantity;
+
+	}
+
+	// encapsulamento getters e setters
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+
+	// metodo comum com tipo de retorno
 	public double totalValueInStock() {
 		return price * quantity;
 	}
@@ -23,12 +68,7 @@ public class Product {
 	}
 
 	public String toString() {
-		return name 
-				+ ", $ " 
-				+ String.format("%.2f", price) 
-				+ ", " 
-				+ quantity 
-				+ " units, Total: $ " 
+		return name + ", $ " + String.format("%.2f", price) + ", " + quantity + " units, Total: $ "
 				+ String.format("%.2f", totalValueInStock());
 	}
 
