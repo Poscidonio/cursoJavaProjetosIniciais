7f3f24dfab0c9d58a808ac2dd68f6ce743c13457
Poscidonio <guilhermeposcidonio@gmail.com>
2022-05-15 17:35:51 -0300

    Exemplo Date

---
+++ exemplo_date/src/application/Program.java
@@ -0,0 +1,73 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+
+		SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
+		SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		SimpleDateFormat sdf3 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		sdf3.setTimeZone(TimeZone.getTimeZone("GMT"));
+		// data do sistema
+		Date x1 = new Date();
+		Date x2 = new Date(System.currentTimeMillis());
+		// data inicial meia noite porem como o sistema de hora local é atrasado 3 horas
+		// ele imprime antes do horario que se é usado
+		Date x3 = new Date(0L);
+		// imprime 5 horas apos a criação do horario
+		Date x4 = new Date(1000L * 60L * 60L * 5L);
+		Date y1 = sdf1.parse("25/06/2018");
+		Date y2 = sdf2.parse("25/06/2018 15:42:07");
+		Date y3 = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+		System.out.println("x1: " + x1);
+		System.out.println("x2: " + x2);
+		System.out.println("x3: " + x3);
+		System.out.println("x4: " + x4);
+		System.out.println("y1: " + y1);
+		System.out.println("y2: " + y2);
+		System.out.println("y3: " + y3);
+		System.out.println("-------------");
+		System.out.println("x1: " + sdf2.format(x1));
+		System.out.println("x2: " + sdf2.format(x2));
+		System.out.println("x3: " + sdf2.format(x3));
+		System.out.println("x4: " + sdf2.format(x4));
+		System.out.println("y1: " + sdf2.format(y1));
+		System.out.println("y2: " + sdf2.format(y2));
+		System.out.println("y3: " + sdf2.format(y3));
+		System.out.println("-------------");
+		System.out.println("x1: " + sdf3.format(x1));
+		System.out.println("x2: " + sdf3.format(x2));
+		System.out.println("x3: " + sdf3.format(x3));
+		System.out.println("x4: " + sdf3.format(x4));
+		System.out.println("y1: " + sdf3.format(y1));
+		System.out.println("y2: " + sdf3.format(y2));
+		System.out.println("y3: " + sdf3.format(y3));
+
+		// *** Manipulando dates ***
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date d = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+		System.out.println(sdf.format(d));
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(d);
+		// adiciona 4 hr a data setada em d
+		cal.add(Calendar.HOUR_OF_DAY, 4);
+		// adiciona em minutes os muinutos de cal
+		int minutes = cal.get(Calendar.MINUTE);
+		// adiciona os meses em month de cal adicionando mais um ja que para o calendar
+		// janeiro é == 0 entao para que imprima corretamente adiciona 1
+		int month = 1 + cal.get(Calendar.MONTH);
+		d = cal.getTime();
+
+		System.out.println(sdf.format(d));
+		System.out.println("Minutes: " + minutes);
+		System.out.println("Month: " + month);
+	}
+
+}
